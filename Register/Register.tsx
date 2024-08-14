84bcb42fd94a7da631db25cf991be53f5143e336
maloth-29 <vinaymaloth29@gmail.com>
2024-08-14 12:21:21 +0530

    first commit

---
+++ Register/Register.tsx
@@ -0,0 +1,52 @@
+"use client"
+import React, { useState } from 'react'
+
+export const Register = () => {
+    const [data,setData]=useState({})
+    const fnRegister=async()=>{
+        try{
+            var dataObj={
+            "data":data
+        }
+           const res= await fetch('http://localhost:2020/std/register',{
+            method:'post',
+            headers:{
+                "content-type":"application/json"
+            },
+            body:JSON.stringify(dataObj)
+           })
+           const result =await res.json() //give mee promis
+           const { acknowledged, insertedId } = result;
+            if (acknowledged && insertedId) {
+                alert('success')
+            } else {
+                alert('fail')
+            }
+           console.log(result)
+        }catch(exe){
+            console.error(exe)
+        }
+    }
+    const  handlechange=(eve:any)=>{
+        const{name,value}=eve.target;
+        setData({...data,[name]:value})
+    }
+  return (
+    <div>
+        <h3>Register</h3>
+        <p>
+            <b>Name:</b><input name='name' onChange={handlechange}/>
+        </p>
+        <p>
+            <b>Roll no:</b><input name='rno' type='number' onChange={handlechange}/>
+        </p>
+        <p>
+            <b>Location:</b><textarea name='loc' onChange={handlechange}/>
+        </p>
+        <p>
+            <button onClick={fnRegister}>register</button>
+        </p>
+        
+    </div>
+  )
+}
